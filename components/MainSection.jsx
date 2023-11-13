b43c2192cd040fc08b74b123e8076dbaedfc6e3d
fariasagustin3 <fariasagustin3@gmail.com>
2023-11-13 19:39:51 -0300

    main section desktop and mobile done

--- components/MainSection.jsx
+++ components/MainSection.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
+import MainImage from '../assets/main_image.png';
+import Image from 'next/image';
 
 const content = {
   title: "Live at your own pace",
+  titleSm: "Rule the world as a Professional Real Estate Consultant",
   description: '"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire."',
 }
 
 const MainSection = () => {
   return (
-    <section 
-      className='px-7 flex flex-col justify-content items-center bg-[#1E3446] py-20' 
+    <section
+      className='px-7 flex flex-col justify-center items-center bg-[#1E3446] h-full py-24 sm:items-start sm:py-0 sm:px-0 sm:bg-white'
       style={{ borderBottomRightRadius: 170 }}
     >
-      <h1 className='text-white font-semibold text-center text-5xl leading-relaxed mb-3'>{content.title}</h1>
-      <p className='text-center text-white'>{content.description}</p>
-      <div className='flex flex-row gap-5 mt-5'>
-        <button 
-          className='text-sm text-white text-center bg-[#F58634] rounded-xl px-5 py-3'
-        >
-          Get Started
-        </button>
-        <button 
-          className='text-sm text-[#F58634] border border-1 border-[#F58634] text-center bg-transparent rounded-xl px-5 py-3'
-        >
-          Learn More
-        </button>
+      <div className='sm:flex sm:flex-col sm:items-start sm:bg-[#1E3446] sm:h-screen sm:py-20 sm:w-4/5 sm:px-14 rounded-br-[500px]'>
+        <h1 className='text-white font-semibold text-center text-5xl leading-relaxed mb-3 sm:hidden'>{content.title}</h1>
+        <h1 className='text-white font-semibold text-center text-5xl leading-relaxed mb-3 hidden sm:text-start w-2/3'>{content.titleSm}</h1>
+        <p className='text-center text-white sm:hidden'>{content.description}</p>
+        <div className='flex flex-row gap-5 mt-5 justify-center'>
+          <button
+            className='text-sm text-white text-center bg-[#F58634] rounded-xl px-5 py-3'
+          >
+            Get Started
+          </button>
+          <button
+            className='text-sm text-[#F58634] border border-1 border-[#F58634] text-center bg-transparent rounded-xl px-5 py-3'
+          >
+            Learn More
+          </button>
+        </div>
+      </div>
+      <div className='hidden sm:flex'>
+        <Image src={MainImage} className='absolute w-96 top-28 right-28 z-10' />
       </div>
     </section>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,9 @@ const content = {
 const Navbar = () => {
   return (
     <section className='flex flex-row items-center justify-between bg-[#1E3446] px-10 py-5 sm:px-0 sm:py-0 sm:bg-white '>
-      <div className='sm:flex sm: flex-row sm:w-4/5 sm:h-full sm:justify-between sm:items-center sm:bg-[#1E3446] sm:py-5 sm:px-14'>
+      
+      {/* logo with links */}
+      <div className='sm:flex sm:flex-row sm:w-4/5 sm:h-full sm:justify-between sm:items-center sm:bg-[#1E3446] sm:py-5 sm:px-14'>
         <Image src={content.logo} className='w-24 sm:pl-20 sm:w-48' alt="" />
         <ul className='hidden sm:flex sm:flex-row sm:items-center sm:gap-5'>
           {content.links.map((link, index) => (
@@ -19,10 +21,14 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
+
+      {/* buttons login and register (desktop version) */}
       <div className='hidden sm:flex sm:flex-row sm:items-center sm:pr-10'>
         <button className='sm:bg-transparent sm:text-black sm:px-4 sm:border-r-[1px] sm:border-r-[#D4D4D4] sm:text-sm sm:font-bold'>Login</button>
         <button className='sm:bg-transparent sm:text-[#F58634] sm:px-4 sm:text-sm sm:font-bold'>Register</button>
       </div>
+
+      {/* button to access menu (mobile version) */}
       <button
         className='bg-transparent text-[#F58634] border border-1 border-[#F58634] px-5 py-3 rounded-xl sm:hidden'
       >
